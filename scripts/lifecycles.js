43e9b46661dd983b8e0f4f0f47c5409361ffefa2
Kevin Alford <zeroeth@pixelflow.org>
2015-04-17 11:45:02 -0500

    Bigger spread between kingdoms

--- scripts/lifecycles.js
+++ scripts/lifecycles.js
@@ -7,7 +7,7 @@ var lifecycles_begin = function()
 	var kingdoms = [];
 
 	// Mass in kg, lifespan in years
-	kingdoms.push(new Kingdom("animal",   {biomass: [0.1,    5000    ], lifespan: [ 0.1,    150   ]}));
+	kingdoms.push(new Kingdom("animal",   {biomass: [0.1,    5000    ], lifespan: [ 0.1,    100   ]}));
 	kingdoms.push(new Kingdom("plant",    {biomass: [0.1,   10000    ], lifespan: [50,     1000   ]}));
 	kingdoms.push(new Kingdom("bacteria", {biomass: [0.001,     0.002], lifespan: [ 0.001,    0.01]}));
 
@@ -34,7 +34,7 @@ var lifecycles_begin = function()
 	// Show population event (death/birth)
 	var show_births = function(population, species)
 	{
-		console.log("birth", population.count_ever, species.name);
+		console.log("BIRTHS", population.count_ever, species.name);
 	}
 
 	for(var population in populations)
