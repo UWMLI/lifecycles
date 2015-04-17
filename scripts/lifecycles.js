04b4a330b63eda9d32b6c678b0d9ff581505bd15
Kevin Alford <zeroeth@pixelflow.org>
2015-04-17 12:02:23 -0500

    More species subrates

--- scripts/lifecycles.js
+++ scripts/lifecycles.js
@@ -7,9 +7,10 @@ var lifecycles_begin = function()
 	var kingdoms = [];
 
 	// Mass in kg, lifespan in years
-	kingdoms.push(new Kingdom("animal",   {biomass: [0.1,    5000    ], lifespan: [ 0.1,    100   ]}));
-	kingdoms.push(new Kingdom("plant",    {biomass: [0.1,   10000    ], lifespan: [50,     1000   ]}));
-	kingdoms.push(new Kingdom("bacteria", {biomass: [0.001,     0.002], lifespan: [ 0.001,    0.01]}));
+	kingdoms.push(new Kingdom("animal",   {biomass: [   0.1,    2000    ], lifespan: [ 1,      100   ]}));
+	kingdoms.push(new Kingdom("plant",    {biomass: [1000,     10000    ], lifespan: [50,     1000   ]}));
+	kingdoms.push(new Kingdom("plant",    {biomass: [   0.1,      10    ], lifespan: [ 1,      100   ]}));
+	kingdoms.push(new Kingdom("bacteria", {biomass: [   0.001,     0.002], lifespan: [ 0.001,    0.01]}));
 
 
 	var species = [];
